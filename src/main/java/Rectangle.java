d0c8b394e306d0f74ac8f905d9a47cfe078feb81
MANSI069 <mmansi069@gmail.com>
2022-07-12 10:15:32 +0530

    second commit

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,14 @@
+public class Rectangle {
+
+    private final int breadth;
+    private final int length;
+
+    public Rectangle(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int perimeter() {
+        return 10;
+    }
+}
