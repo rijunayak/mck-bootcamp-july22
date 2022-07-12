72fe8523e1132a3ad4398bd68aa8f48380c9620b
Biman <biman20006@iiitd.ac.in>
2022-07-12 10:49:28 +0530

    [Adarsh | Biman] Add Perimeter Fourteen when length three and breadth four

--- src/main/java/RectanglePerimeter.java
+++ src/main/java/RectanglePerimeter.java
@@ -3,13 +3,12 @@ public class RectanglePerimeter {
     private int length;
     private int breadth;
 
-    public RectanglePerimeter(int length ,int breadth)
-    {
+    public RectanglePerimeter(int length, int breadth) {
         this.length = length;
         this.breadth = breadth;
     }
 
-    public int perimeter(){
-        return 10;
+    public int perimeter() {
+        return 2*(length + breadth);
     }
 }
--- src/test/java/RectanglePerimeterTest.java
+++ src/test/java/RectanglePerimeterTest.java
@@ -2,18 +2,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectanglePerimeterTest {
 
     @Test
     @DisplayName("The perimeter should be ten when length is two and breath is three")
-    public void perimeterShouldBeTenWhenLengthTwoBreadthThree(){
+    public void perimeterShouldBeTenWhenLengthTwoBreadthThree() {
         // Arrange
-        RectanglePerimeter rectanglePerimeter = new RectanglePerimeter(2,3);
+        RectanglePerimeter rectanglePerimeter = new RectanglePerimeter(2, 3);
 
         //Act
         int perimeter = rectanglePerimeter.perimeter();
 
         //Assert
-        Assertions.assertEquals(10,perimeter);
+        assertEquals(10, perimeter);
     }
+
+    @Test
+    @DisplayName("The perimeter should be Fourteen when length is three and breath is four")
+    public void perimeterShouldBeFourteenWhenLengthThreeBreadthFour() {
+        // Arrange
+        RectanglePerimeter rectanglePerimeter = new RectanglePerimeter(3, 4);
+
+        //Act
+        int perimeter = rectanglePerimeter.perimeter();
+
+        //Assert
+        assertEquals(14, perimeter);
+    }
+
+
+
 }
